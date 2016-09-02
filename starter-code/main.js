ef9d30a3bf9274239805db24fc14cc7de9e56041
Jacob Elder <jacobelderonly@gmail.com>
2016-09-01 22:55:54 -0700

    added cards using js

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,3 +1,4 @@
+/*
 console.log("JS file is connected to HTML! Woo!")
 
 var cardOne = "queen";
@@ -5,8 +6,25 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+
 if (cardTwo === cardFour) {
 alert("You found a match!")
 } else {
 	alert ("Sorry, try again.")
 };
+*/
+
+var gameBoard = document.getElementById('game-board');
+
+
+var createBoard = function() {
+
+for(var i=0;i<4;i++) {
+	var cardElement=document.createElement('div');
+	cardElement.className = 'card';
+	gameBoard.appendChild(cardElement);
+}
+
+};
+
+createBoard();
