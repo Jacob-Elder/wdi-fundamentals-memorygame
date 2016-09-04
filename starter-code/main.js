939ef989606902d8781898e88737c85503fec706
Jacob Elder <jacobelderonly@gmail.com>
2016-09-03 23:31:49 -0700

    added all images, made seperate html pages, and finished javascript.

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
 console.log("JS file is connected to HTML! Woo!")
 
@@ -10,21 +11,90 @@ var cardFour = "king";
 if (cardTwo === cardFour) {
 alert("You found a match!")
 } else {
-	alert ("Sorry, try again.")
+ alert ("Sorry, try again.")
 };
 */
+// A counter to keep track of players score
+var score = 0;
+
+
+// An empty placeholder for what will be our cards
+var cardElement;
+
+
+function flipCard() {
+		if (this.getAttribute('data-card') === 'king'){
+		this.innerHTML = "<img src='king_img.jpg'>";
+	} else {
+		this.innerHTML = "<img src='queen_img.jpg'>";
+	}
+}
+
+var cardsInPlay = [];
+
+var isMatch = function () {
+	if (cardsInPlay[0] == cardsInPlay[1]) {
+		score++;
+ 		return('Great! Thats a match!!!')
+	} else {
+		score--;
+ 		return("Uh Oh! The cards don't match.");
+	}
+};
+
+var isTwoCards = function() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+
+	if(cardsInPlay.length==2) {
+		return(isMatch());
+	}
+
+	cardsInPlay = [];
+};
 
 var gameBoard = document.getElementById('game-board');
 
+var deck = ['queen','queen','king','king'];
 
-var createBoard = function() {
+function shuffle(deck) {
+  var currentIndex = deck.length, temporaryValue, randomIndex;
 
-for(var i=0;i<4;i++) {
-	var cardElement=document.createElement('div');
-	cardElement.className = 'card';
-	gameBoard.appendChild(cardElement);
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = deck[currentIndex];
+    deck[currentIndex] = deck[randomIndex];
+    deck[randomIndex] = temporaryValue;
+  }
+
+  return deck;
 }
 
+var createBoard = function() {
+	var cards = shuffle(deck);
+	for(var i=0;i<cards.length;i++) {
+		cardElement=document.createElement('div');
+		cardElement.className = 'card';
+		gameBoard.appendChild(cardElement);
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', flipCard);
+		cardElement.addEventListener('click', isTwoCards);
+	}
+};
+
+var isTwoCards = function() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	if(cardsInPlay.length==2) {
+		alert(isMatch() + " Your total score is " + score);
+		gameBoard.innerHTML = "";
+		createBoard();
+		cardsInPlay = [];
+	}
 };
 
 createBoard();
